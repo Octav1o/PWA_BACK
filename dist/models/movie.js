729872d214649ecd6b5f8d2ae3899747a82d36b3
Octavio Buenfil <106894759+Octav1o@users.noreply.github.com>
2024-10-24 11:13:55 -0600

    UPDATE

---
+++ dist/models/movie.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Movie {
+    constructor(name, year, category, id) {
+        this.name = name;
+        this.year = year;
+        this.category = category;
+        this.id = id;
+    }
+}
+exports.default = Movie;
---
+++ dist/movies/movies.routes.js
@@ -0,0 +1,98 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.moviesRouter = void 0;
+const express_1 = __importDefault(require("express"));
+const mongodb_1 = require("mongodb");
+const database_service_1 = require("../services/database.service");
+exports.moviesRouter = express_1.default.Router();
+exports.moviesRouter.use(express_1.default.json());
+exports.moviesRouter.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const movies = (yield ((_a = database_service_1.collections.movies) === null || _a === void 0 ? void 0 : _a.find({}).toArray()));
+        res.status(200).send(movies);
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+}));
+exports.moviesRouter.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c;
+    const id = (_a = req === null || req === void 0 ? void 0 : req.params) === null || _a === void 0 ? void 0 : _a.id;
+    try {
+        const query = { _id: new mongodb_1.ObjectId(id) };
+        const movie = (yield ((_b = database_service_1.collections.movies) === null || _b === void 0 ? void 0 : _b.findOne(query)));
+        if (movie) {
+            res.status(200).send(movie);
+        }
+        else {
+            res.status(404).send(`Unable to find matching document with id : ${(_c = req === null || req === void 0 ? void 0 : req.params) === null || _c === void 0 ? void 0 : _c.id}`);
+        }
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+}));
+exports.moviesRouter.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const newMovie = req.body;
+        const result = yield ((_a = database_service_1.collections.movies) === null || _a === void 0 ? void 0 : _a.insertOne(newMovie));
+        result
+            ? res.status(201).send(`Successfully created a new movie with id ${result.insertedId}`)
+            : res.status(500).send("Failed to create a new movie");
+    }
+    catch (error) {
+        console.error(error);
+        res.status(400).send(error.message);
+    }
+}));
+exports.moviesRouter.put("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
+    const id = (_a = req === null || req === void 0 ? void 0 : req.params) === null || _a === void 0 ? void 0 : _a.id;
+    try {
+        const updateMovie = req.body;
+        const query = { _id: new mongodb_1.ObjectId(id) };
+        const result = yield ((_b = database_service_1.collections.movies) === null || _b === void 0 ? void 0 : _b.updateOne(query, { $set: updateMovie }));
+        result
+            ? res.status(200).send(`Successfully updated movie with id ${id}`)
+            : res.status(304).send(`Movie with id: ${id} not updated`);
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+}));
+exports.moviesRouter.delete("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
+    const id = (_a = req === null || req === void 0 ? void 0 : req.params) === null || _a === void 0 ? void 0 : _a.id;
+    try {
+        const query = { _id: new mongodb_1.ObjectId(id) };
+        const result = yield ((_b = database_service_1.collections.movies) === null || _b === void 0 ? void 0 : _b.deleteOne(query));
+        if (result && result.deletedCount) {
+            res.status(202).send(`Successfully deleted movie with id ${id}`);
+        }
+        else if (!result) {
+            res.status(400).send(`Failed to remoive movie with id : ${id}`);
+        }
+        else if (!result.deletedCount) {
+            res.status(404).send(`Movie with id: ${id} not found`);
+        }
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+}));
